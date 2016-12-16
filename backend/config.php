275913dea8983feb23ccfd119939a53aea405b6a
Christoph Haas <christoph.h@sprinternet.at>
2016-12-16 14:51:48 +0100

    Line endings

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,8 @@
-<?php// config options$ADMINUSERNAME = "admin";$ADMINPASSWORD = "admin";$SERVER = "file:///var/run/zarafa";$CALDAVURL = "http://localhost:8080/ical/";$TEMPDIR = "/tmp/";?>
+<?php
+// config options
+$ADMINUSERNAME = "admin";
+$ADMINPASSWORD = "admin";
+$SERVER = "file:///var/run/zarafa";
+$CALDAVURL = "http://localhost:8080/ical/";
+$TEMPDIR = "/tmp/";
+?>
