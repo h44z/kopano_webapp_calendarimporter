611b5162ccecc3a071ad8e9b60f91ff093e5b841
Christoph Haas <christoph.h@sprinternet.at>
2017-05-31 14:09:22 +0200

    Update config.php for kopano compatibility (this breaks the plugin for zarafa!)

--- backend/config.php
+++ backend/config.php
@@ -2,6 +2,6 @@
 // config options
 $ADMINUSERNAME = "admin";                   // Kopano administrative user
 $ADMINPASSWORD = "admin";                   // Kopano administrative user password
-$SERVER = "file:///var/run/zarafa";         // Kopano socket or http(s) connection
+$SERVER = "http://localhost:236/kopano";         // Kopano socket or http(s) connection
 $CALDAVURL = "http://localhost:8080/ical/"; // Caldav URL
 $TEMPDIR = "/tmp/";                         // Temporary directory for storing downloaded ics files
